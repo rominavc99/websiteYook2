28d292369272a6a457a51ea26b3638fc8fd0a5ac
rominavc99 <procrom1999@gmail.com>
2021-03-25 12:41:01 -0700

    repo actualizado

--- public/javascript.js
+++ public/javascript.js
@@ -11,4 +11,16 @@ jQuery(window).scroll(function() {
 });
 
 
-
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
